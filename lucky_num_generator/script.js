fb44eec6dc60398607809204a2a09482068638b2
AVINASH PANDEY <98386256+Avi2492@users.noreply.github.com>
2023-05-29 11:57:05 +0530

    Add files via upload

---
+++ lucky_num_generator/script.js
@@ -0,0 +1,8 @@
+function generateLuckyNumber() {
+  //   alert("You're inside javascript!");
+
+  //   generate a random number
+  let randomNumber = Math.floor(Math.random() * 1000);
+
+  alert(randomNumber);
+}
